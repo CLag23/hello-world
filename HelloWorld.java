06d90b0913777562b75cd2f562280e4e9e03d34c
Carls Laguerre <166565679+CLag23@users.noreply.github.com>
2025-02-11 11:31:42 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,14 @@
 public class HelloWorld {
    public static void main(String[] args) {
       System.out.println("Hello world!");
-   }
-}
+      
+      // name is a referance variable 
+      String name = "George Washington";
+      System.out.println(name);
+      
+      // age is a primitive vaiable 
+      int age = 24;
+      System.out.println(age);
+      
+   }// end main
+}// end class
